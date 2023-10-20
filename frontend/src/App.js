4bd1ec99b2ea90165234d4f21001c6bdd6d9dabc
Tarachandani <ayush.tarachandani@gmail.com>
2023-10-20 16:03:36 -0500

    changed image background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import FoodImage from './images/imageoffood.jpeg';
 
 function App(){ //Takes care of FileAdder
     return(
-        <div>
+        <div style={{backgroundColor: 'powderblue'}}>
+            
             <h1>AI Recipe App</h1>
             <img src={FoodImage} alt="food on table"></img>
             <br></br>
             <text><text>Please lay out your ingredients (like shown above):</text></text>
-            <FileAdder></FileAdder>
+            <FileAdder ></FileAdder>
         </div>
         
     )
