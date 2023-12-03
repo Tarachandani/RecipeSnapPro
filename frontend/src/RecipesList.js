eb72068a329ffa0eded93ea737f9fbe4cfbafd77
Warel123.R <pranavpull87@gmail.com>
2023-12-02 22:25:22 -0600

    editing frontend

---
+++ frontend/src/RecipesList.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+function RecipesList({ingredientlist}){
+
+    const [ingredients, setIngredients]= useState('');
+    const [meals, setMeals] = useState('');
+
+    const GetMealList=async ()=>{
+        setIngredients(ingredientlist);
+        // ingredients.map((ingredient)=>{
+        //     return <div>
+
+        //     </div>
+        // })
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredients}`)
+        .then(response => response.json())
+        .then((data)=>{
+            setMeals(data.meals);
+        });
+
+
+    }
+
+    
+    return(
+        <div>
+            <button onClick={GetMealList}>hello</button>
+            {meals}
+        </div>
+        
+    )
+}
+export default RecipesList;
